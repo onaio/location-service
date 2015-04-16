438ce885690228d8ac8a6c4ebacd091e8274455b
geoffreymuchai <geoffreymuchai@gmail.com>
2015-04-16 21:29:56 +0300

    GM: Remove trailing slash in api endpoint

--- onalocation/api/urls.py
+++ onalocation/api/urls.py
@@ -4,12 +4,12 @@ from api.viewsets import LocationViewSet
 
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'locations', LocationViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [    
+urlpatterns = [
     url(r'^api/', include(
         'rest_framework.urls', namespace='rest_framework'))
 ]
