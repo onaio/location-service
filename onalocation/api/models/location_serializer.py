a8a960b2499225abf6af329c08f08210f2f17ac8
geoffreymuchai <geoffreymuchai@gmail.com>
2015-04-21 10:02:01 +0300

    GM: Children are returned as a map of locations

--- onalocation/api/models/location_serializer.py
+++ onalocation/api/models/location_serializer.py
@@ -5,7 +5,17 @@ from ona.models import Location
 
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
     location_type = serializers.CharField(max_length=200)
+    children = serializers.SerializerMethodField('get_children_map')
 
     class Meta:
         model = Location
         fields = ('name', 'code', 'children', 'metadata', 'location_type')
+
+    def get_children_map(self, obj):
+        if obj:
+            children = [{'id': c.id,
+                        'name': c.name,
+                        'code': c.code,
+                        'meta': c.metadata} for c in obj.children.all()]
+
+            return children
